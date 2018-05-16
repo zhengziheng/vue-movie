993ec2047040e841a91ffe30df70e4d505666eaa
zhengziheng <33058602+zhengziheng@users.noreply.github.com>
2018-05-16 11:33:57 +0800

    movieList-init

---
+++ src/components/movie-list/movie-list.vue
@@ -0,0 +1,36 @@
+<!-- 电影列表组件 -->
+<template>
+  <div class="movie-list">
+    <ul>
+      <li
+      v-for="(movie,index) in movies"
+      :key="index"
+      @click="selectItem(movie)"
+      ref="group">
+      <div class="date" v-if="needDate && !dateEqual(index)">{{movie.date}}</div>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+name:'',
+
+  data () {
+    return {
+    };
+  },
+
+props:{},
+  components: {},
+
+  computed: {},
+
+
+  methods: {}
+}
+
+</script>
+<style lang='stylus' scoped>
+</style>
