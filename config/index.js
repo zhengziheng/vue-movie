387ec953ed23b8f91d28801f3ff4ce4ada0a1389
zhengziheng <33058602+zhengziheng@users.noreply.github.com>
2018-05-17 11:27:28 +0800

    movieShowApi-finish

--- config/index.js
+++ config/index.js
@@ -10,7 +10,16 @@ module.exports = {
     // Paths
     assetsSubDirectory: 'static',
     assetsPublicPath: '/',
-    proxyTable: {},
+    proxyTable: {
+      '/v2':{
+        target:'http://api.douban.com',
+        changeOrigin:true,
+        pathRewrite:{
+          '^/v2':'/v2'
+
+        }
+      }
+    },
 
     // Various Dev Server settings
     host: 'localhost', // can be overwritten by process.env.HOST
