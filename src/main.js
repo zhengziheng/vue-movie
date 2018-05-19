e58fbc1e2cc63a1f52995257f0bb87fbfccc94b0
zhengziheng <33058602+zhengziheng@users.noreply.github.com>
2018-05-19 17:29:17 +0800

    vuex-init

--- src/main.js
+++ src/main.js
@@ -3,13 +3,19 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
-
+import store from './store'
+import VueLazyLoad from 'vue-lazyload'
 Vue.config.productionTip = false
 
+Vue.use(VueLazyLoad,{
+  loading:require('./common/image/loading.gif')
+})
+import 'common/stylus/index.styl'
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   router,
+  store,
   components: { App },
   template: '<App/>'
 })
---
+++ src/store/state.js
@@ -0,0 +1,6 @@
+const state = {
+  movies:{},
+}
+
+
+export default state
